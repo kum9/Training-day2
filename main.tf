ab1aa81b23b15009d967eedbeeb47ca4ed8b9317
kum9 <etiennekum12@gmail.com>
2025-03-13 00:13:41 -0600

    Name change of ec2

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t3.micro"
   tags = {
-    Name = "HelloWorld"
+    Name = "updated-terraform-instance"
   }
 }
