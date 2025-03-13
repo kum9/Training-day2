0ffc4538ae35b1633ef2d5e059c37e3c2fc22640
kum9 <etiennekum12@gmail.com>
2025-03-13 17:24:53 -0600

    backend updated

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "terraform-state-cicd1234"
-        key    = "terraform.tfstate/"
+        key    = "terraform.tfstate"
         region = "us-east-1"
     }
 }
